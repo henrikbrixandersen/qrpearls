50cbaecd881445d550287256326bc99b5d22e060
Henrik Brix Andersen <henrik@brixandersen.dk>
2013-01-25 21:52:21 +0100

    Guard client-side against missing text.

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,8 +1,27 @@
 /**
- * Attach language selection handler.
+ * Language selection handler.
  */
 $(".lang-select").click(function() {
     document.cookie="lang=" + $(this).data('lang');
     location.reload();
     return false;
+});
+
+/**
+ * Alert close button handler.
+ */
+$("#alert-no-text-close").click(function() {
+    $("#alert-no-text").fadeOut();
+});
+
+/**
+ * Form submit handler.
+ */
+$("#form").submit(function() {
+    if ($("#inputText").val().length == 0) {
+      $("#alert-no-text").fadeIn();
+      return false;
+    }
+
+    return true;
 });
