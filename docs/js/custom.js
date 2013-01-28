4afed9146914e72ea4d21ad2676920f6925001dd
Henrik Brix Andersen <henrik@brixandersen.dk>
2013-01-28 15:31:36 +0100

    Ensure the DOM is ready before attaching handlers.

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,32 +1,34 @@
-/**
- * Language selection handler.
- */
-$('.lang-select').click(function() {
-    document.cookie='lang=' + $(this).data('lang');
-    location.reload();
-    return false;
-});
+$(document).ready(function() {
+    /**
+     * Language selection handler.
+     */
+    $('.lang-select').click(function() {
+	document.cookie='lang=' + $(this).data('lang');
+	location.reload();
+	return false;
+      });
 
-/**
- * Alert close button handler.
- */
-$("#alert-no-text-close").click(function() {
-    $('#alert-no-text').fadeOut();
-});
+    /**
+     * Alert close button handler.
+     */
+    $("#alert-no-text-close").click(function() {
+	$('#alert-no-text').fadeOut();
+      });
 
-/**
- * Text typeahead choices.
- */
-$('#inputText').typeahead({source: ['http://', 'https://', 'mailto:', 'tel:', 'sms:', 'mms:']});
+    /**
+     * Text typeahead choices.
+     */
+    $('#inputText').typeahead({source: ['http://', 'https://', 'mailto:', 'tel:', 'sms:', 'mms:']});
 
-/**
- * Form submit handler.
- */
-$('#form').submit(function() {
-    if ($('#inputText').val().length == 0) {
-      $('#alert-no-text').fadeIn();
-      return false;
-    }
+    /**
+     * Form submit handler.
+     */
+    $('#form').submit(function() {
+	if ($('#inputText').val().length == 0) {
+	  $('#alert-no-text').fadeIn();
+	  return false;
+	}
 
-    return true;
-});
+	return true;
+      });
+  });
