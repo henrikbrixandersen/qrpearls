e01033e22353d5ac01f0f9bd6c6dc32ad4814b96
Henrik Brix Andersen <henrik@brixandersen.dk>
2013-01-28 11:03:03 +0100

    Expand language and page parameters.

--- docs/index.pl
+++ docs/index.pl
@@ -10,16 +10,34 @@ use Locale::TextDomain;
 use Imager::QRCode;
 use SVG qw/-nocredits => 1/;
 
+# Directories
 my $tmpldir = "$ENV{BASE_DIRECTORY}/tmpl";
 
-my $charset = 'UTF-8';
-my @langs = qw/en da/;
+# Pages
+my $pages = {
+    home => {
+        path => '/',
+        name => __('Home'),
+        icon => 'home',
+    },
+    about => {
+        name => __('About'),
+        icon => 'book',
+    }
+};
+my @pageorder = qw/home about/;
+my $page = $pageorder[0];
+
+# Languages
+my $langs = {
+    en => 'English',
+    da => 'Dansk',
+};
 my $lang = 'en';
 
+# Initialize CGI and catch errors
 my $q = CGI->new;
 $q->charset('UTF-8');
-
-# Catch errors
 my $error = $q->cgi_error;
 if ($error) {
     print $q->header(-status => $error);
@@ -27,33 +45,56 @@ if ($error) {
 }
 
 # Determine language
-if ($q->cookie('lang') ~~ @langs) {
+if ($q->cookie('lang') ~~ $langs) {
     $lang = $q->cookie('lang');
 } else {
+    my @available = keys $langs;
     my $accept = I18N::AcceptLanguage->new(defaultLanguage => $lang);
-    my $acceptLang = $accept->accepts($ENV{HTTP_ACCEPT_LANGUAGE}, \@langs);
+    my $acceptLang = $accept->accepts($ENV{HTTP_ACCEPT_LANGUAGE}, \@available);
     $lang = $acceptLang if ($acceptLang);
 }
 
 # Print header
 print $q->header;
 
-# set template lang
+# TODO: set template lang
 
-# check page param, prepare template and present content
+# Determine page
+if ($q->param('page') ~~ $pages) {
+    $page = $q->param('page');
+}
 
+# Load template
 my $tmpl = HTML::Template->new(path => [$tmpldir],
-                               filename => 'index.tmpl',
+                               filename => "$page.tmpl",
                                utf8 => 1,
                                cache => 1);
 
-# Fill in template language parameters
+# Generate template language parameters
 my @langs_loop;
-foreach (@langs) {
-    my $lang_data = { LANG => $_, SELECTED => ($_ eq $lang) };
+foreach (sort keys $langs) {
+    my $lang_data = {
+        LANG => $_,
+        NAME => $langs->{$_},
+        SELECTED => ($_ eq $lang)
+    };
     push @langs_loop, $lang_data;
 }
-$tmpl->param(LANG => $lang, LANGS_LOOP => \@langs_loop);
 
-# Print filled-in template
+# Generate template page parameters
+my @pages_loop;
+foreach (@pageorder) {
+    my $page_data = {
+        PATH => $pages->{$_}->{path} || "/$_/",
+        ICON => $pages->{$_}->{icon},
+        NAME => $pages->{$_}->{name},
+        SELECTED => ($_ eq $page)
+    };
+    push @pages_loop, $page_data;
+}
+
+# Fill in template
+$tmpl->param(LANG => $lang,
+             LANGS_LOOP => \@langs_loop,
+             PAGES_LOOP => \@pages_loop);
 print $tmpl->output;
