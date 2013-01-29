f7cdba77c66ab883951d694fc8f072080051e856
Henrik Brix Andersen <henrik@brixandersen.dk>
2013-01-29 10:11:56 +0100

    Rename input elements to match general naming scheme.

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -18,13 +18,13 @@ $(document).ready(function() {
     /**
      * Text typeahead choices.
      */
-    $('#inputText').typeahead({source: ['http://', 'https://', 'mailto:', 'tel:', 'sms:', 'mms:']});
+    $('#form-text').typeahead({source: ['http://', 'https://', 'mailto:', 'tel:', 'sms:', 'mms:']});
 
     /**
      * Form submit handler.
      */
     $('#form').submit(function() {
-	if ($('#inputText').val().length == 0) {
+	if ($('#form-text').val().length == 0) {
 	  $('#alert-no-text').fadeIn();
 	  return false;
 	}
