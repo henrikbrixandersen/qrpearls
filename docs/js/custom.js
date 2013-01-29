16305a0dc44d7d8f9fddb2b848b5f3563b12bd30
Henrik Brix Andersen <henrik@brixandersen.dk>
2013-01-29 10:25:18 +0100

    Only autofocus input field on non-touch devices.

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,33 +1,34 @@
+/**
+ * This is where it all begins.
+ */
 $(document).ready(function() {
-    /**
-     * Hide address bar on iOS devices.
-     */
+    // Browser touch event support
+    var touchSupported = isMouseEventSupported('touchmove');
+
+    // Hide address bar on iOS devices.
     setTimeout('window.scrollTo(0, 0);', 0);
 
-    /**
-     * Language selection handler.
-     */
+    // Language selection handler.
     $('.lang-select').click(function() {
 	document.cookie='lang=' + $(this).data('lang');
 	location.reload();
 	return false;
       });
 
-    /**
-     * Alert close button handler.
-     */
+    // Alert close button handler.
     $("#alert-no-text-close").click(function() {
 	$('#alert-no-text').fadeOut();
       });
 
-    /**
-     * Text typeahead choices.
-     */
-    $('#form-text').typeahead({source: ['http://', 'https://', 'mailto:', 'tel:', 'sms:', 'mms:']});
+    // Text typeahead choices.
+    $('#form-text').typeahead({source: ['http://',
+					'https://',
+					'mailto:',
+					'tel:',
+					'sms:',
+					'mms:']});
 
-    /**
-     * Form submit handler.
-     */
+    // Form submit handler.
     $('#form').submit(function() {
 	if ($('#form-text').val().length == 0) {
 	  $('#alert-no-text').fadeIn();
@@ -36,4 +37,28 @@ $(document).ready(function() {
 
 	return true;
       });
+
+    // Autofocus input field on non-touch devices.
+    if (!touchSupported) {
+      $('#form-text').focus();
+    }
   });
+
+
+/**
+ * Function to test whether a given mouse event is supported.
+ */
+function isMouseEventSupported(eventName) {
+  var eventName = 'on' + eventName;
+  var el = document.createElement('div');
+  var isSupported = (eventName in el);
+
+  if (!isSupported) {
+    el.setAttribute(eventName, 'return;');
+    isSupported = typeof el[eventName] == 'function';
+  }
+
+  el = null;
+
+  return isSupported;
+}
