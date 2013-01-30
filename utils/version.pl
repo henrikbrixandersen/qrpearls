428e784c95298a7cd80e06da4e3c4c2898f163ac
Henrik Brix Andersen <henrik@brixandersen.dk>
2013-01-30 11:02:07 +0100

    Take margin into account.

--- utils/version.pl
+++ utils/version.pl
@@ -2,7 +2,8 @@
 
 use strict;
 
-my $size = 21;
+# Version = 1 + margin
+my $size = 21 + 2;
 foreach my $i (1..40) {
     print "              <option value=\"$i\">$i (${size}x${size})</option>\n";
     $size += 4;
